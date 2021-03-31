18cd5c07938b272bdbfe69b4972356625accf245
piotr21t <piotr.trzeciak@poczta.fm>
2021-03-31 18:49:43 +0200

    dodaje plik main

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("TEST");
+        System.out.println("Mój pierwszy commit");
+	// write your code here
+    }
+}
