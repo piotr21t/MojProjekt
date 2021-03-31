10e7c9face4ecf6050cf7e8453ee142a8756087c
piotr21t <piotr.trzeciak@poczta.fm>
2021-03-31 19:21:28 +0200

    commit1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("TEST");
         System.out.println("Mój pierwszy commit");
+        System.out.println("commit");
 	// write your code here
     }
 }
